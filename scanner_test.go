8e88c6a9529f7d527621c95dcb299591c0f97267
aereal <aereal@aereal.org>
2016-12-29 17:33:29 +0900

    Match against all tokens

--- scanner_test.go
+++ scanner_test.go
@@ -1,30 +1,85 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
 
-type expectation struct {
-	input   string
+type result struct {
 	token   Token
 	literal string
 }
 
+type expectation struct {
+	input   string
+	results []result
+}
+
 func TestScanner_Scan(t *testing.T) {
 	var expectations = []expectation{
-		{input: ``, token: EOF},
-		{input: "hello", token: PARAGRAPH, literal: "hello"},
-		{input: "hello\nworld", token: PARAGRAPH, literal: "hello\nworld"},
+		{
+			input: ``,
+			results: []result{
+				{token: EOF},
+			},
+		},
+		{
+			input: "hello",
+			results: []result{
+				{token: PARAGRAPH, literal: "hello"},
+				{token: EOF},
+			},
+		},
+		{
+			input: "hello\nworld",
+			results: []result{
+				{token: PARAGRAPH, literal: "hello\nworld"},
+				{token: EOF},
+			},
+		},
 	}
 
-	for i, expect := range expectations {
+	for _, expect := range expectations {
 		scanner := NewScanner(strings.NewReader(expect.input))
-		token, literal := scanner.Scan()
-		if expect.token != token {
-			t.Errorf("%d %q token mismatched; expected=%q actual=%q <%q>", i, expect.input, expect.token, token, literal)
-		} else if expect.literal != literal {
-			t.Errorf("%d %q literal mismatched; expected=%q actual=%q", i, expect.input, expect.literal, literal)
+		results := slurpTokens(scanner)
+		ok, errorMsg := matchTokens(expect.results, results)
+		if !ok {
+			t.Error(errorMsg)
 		}
 	}
+}
+
+func slurpTokens(s *Scanner) (results []result) {
+	for {
+		token, literal := s.Scan()
+		result := result{token: token, literal: literal}
+		results = append(results, result)
+		if token == EOF {
+			break
+		}
+	}
+	return
+}
+
+func matchTokens(expected []result, actual []result) (ok bool, msg string) {
+	if len(expected) != len(actual) {
+		ok = false
+		msg = fmt.Sprintf("Number of tokens mismatched; expected=%d actual=%d <%q>", len(expected), len(actual), actual)
+		return
+	}
+	for i, _ := range expected {
+		if expected[i].token != actual[i].token {
+			ok = false
+			msg = fmt.Sprintf("Token mismatched; expected=%q actual=%q <%q>", expected[i].token, actual[i].token, actual[i].literal)
+			return
+		}
+		if expected[i].literal != actual[i].literal {
+			ok = false
+			msg = fmt.Sprintf("Literal mismatched; expected=%q actual=%q", expected[i].literal, actual[i].literal)
+			return
+		}
+	}
+	ok = true
+	return
 }
