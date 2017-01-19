c133462f7bd22aac4d8010a36f867ab6d7e15528
aereal <aereal@aereal.org>
2017-01-19 21:27:15 +0900

    parser test

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ build: parser.go
 
 parser.go: parser.go.y
 	go tool yacc -o parser.go parser.go.y
+
+test: parser.go
+	go test -v ./...
---
+++ parser_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+type expectation struct {
+	input  string
+	result []Block
+}
+
+func TestParser_Parse(t *testing.T) {
+	var expectations = []expectation{
+		{
+			input: `- a`,
+			result: []Block{
+				UnorderedList{
+					items: []UnorderedListItem{
+						UnorderedListItem{text: "a"},
+					},
+				},
+			},
+		},
+	}
+
+	for i, expect := range expectations {
+		actual, err := Parse(strings.NewReader(expect.input))
+		if err != nil {
+			t.Errorf("! #%d Failed to parse: %q", i, err)
+		}
+		ok, msg := matchResult(expect.result, actual)
+		if !ok {
+			t.Error(msg)
+		}
+	}
+}
+
+func matchResult(expected []Block, actual []Block) (ok bool, msg string) {
+	if len(expected) != len(actual) {
+		ok = false
+		msg = fmt.Sprintf("Number of blocks mismatched; %d expected, but %d got; <%#v>", len(expected), len(actual), actual)
+		return
+	}
+	if !reflect.DeepEqual(expected, actual) {
+		ok = false
+		msg = fmt.Sprintf("Parsed result mismatched\nExpected:\n%#v\nActual:\n%#v\n", expected, actual)
+		return
+	}
+	ok = true
+	return
+}
